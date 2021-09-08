13ba6717bda77bebc7d40dbada20be96645efaf3
Vagner Fabricio <wagnerfabricio@hotmail.com>
2021-09-08 15:55:24 -0300

    Create roll dice function

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-const diceImage = document.getElementById("dice-image");
-const diceGif = document.getElementById("dice-gif");
-const divContainerStatistics = document.getElementsByClassName(
-  "containerStatistics"
-);
-
 /* ----------------- Roll dices function ---------------- */
-function rollDices() {
+function rollDices(n) {
   let dicesArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  for (let i = 1; i <= 1000; i++) {
+  for (let i = 1; i <= n; i++) {
     //   Row 1st dice randomly
     const dice1 = Math.ceil(Math.random(i) * 6);
 
@@ -20,6 +14,9 @@ function rollDices() {
     // Add +1 time this number have been sorted in the array
     dicesArray[result - 2] += 1;
   }
+
+  createStatistics(dicesArray);
+  createGraphic(dicesArray);
 
   return dicesArray;
 }
@@ -43,23 +40,38 @@ function createGraphic(a) {
   for (let i = 0; i < a.length; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("graphic");
-    newDiv.style.width = a[i] + "px";
+    newDiv.style.width = a[i] + 20 + "px";
     newDiv.innerHTML = i + 2 + ": " + a[i];
     containerGraphic.appendChild(newDiv);
   }
 }
 
-/* ------------------ CALL ALL ACTIONS ------------------ */
+/* ------------------ Call actions and show hidden elements-------------- */
 const callActions = () => {
-  rollDices();
-  createStatistics(rollDices());
-  createGraphic(rollDices());
-
+  rollDices(1000);
   const hiddenDivs = document.getElementsByClassName("remove");
   for (let i = 0; i < hiddenDivs.length; i++) {
     hiddenDivs[i].classList.remove("hidden");
   }
 };
 
+/* ------------------- Dices Animation ------------------ */
+const diceImage = document.getElementById("dice-image");
+const diceGif = document.getElementById("dice-gif");
+
+// Start dice animation
+const startAnimation = () => {
+  diceImage.classList.add("hidden");
+  diceGif.classList.remove("hidden");
+};
+
+//Stop dice animation
+const stopAnimation = () => {
+  diceGif.classList.add("hidden");
+  diceImage.classList.remove("hidden");
+};
+
 const rollDicesButton = document.getElementById("roll-dices");
 rollDicesButton.addEventListener("click", callActions);
+rollDicesButton.addEventListener("mouseover", startAnimation);
+rollDicesButton.addEventListener("mouseout", stopAnimation);
